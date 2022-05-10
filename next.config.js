1414ef9b70430bb2a124a8300fbf45b93bd7aece
Ahmed ZAGHLOUL <ahmed.zaghloul@valeo.com>
2022-05-10 15:54:22 +0200

    Edit next config file

--- next.config.js
+++ next.config.js
@@ -4,7 +4,11 @@ const { i18n } = require("./next-i18next.config");
 
 const nextConfig = {
   reactStrictMode: true,
-  i18n,
+  i18n: {
+    locales: ["en", "fr"],
+    defaultLocale: "en",
+    localeDetection: true,
+  },
 };
 
 module.exports = nextConfig;
